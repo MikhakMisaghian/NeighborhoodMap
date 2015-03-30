1b3e68b20a98f611969c4607587d71f1f06034b6
MikhakMisaghian <MikhakMisaghian@users.noreply.github.com>
2015-03-30 14:47:27 -0700

    testing google map display

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var initialCoordinate = {lat: 37.7577, lng: -122.4376};
 function initialize() {
     var mapOptions = {
       center: new google.maps.LatLng(initialCoordinate.lat, initialCoordinate.lng),
-      zoom: 8,
+      zoom: 12,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     return new google.maps.Map(document.getElementById("map-canvas"), mapOptions);

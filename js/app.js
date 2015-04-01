53ed53115cc8e870aa3d9521a471deaba63524ff
Mikhkak Buenaventura <mikhkakbuenaventura@Mikhkaks-MBP.attlocal.net>
2015-04-01 11:27:31 -0700

    display google map using ko applyBindings on window load

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 // Google maps initial cordinates for San Francisco, CA
-var initialCoordinate = {lat: 37.7577, lng: -122.4376, searchQuery: 'landmarks'};
+var initialCoordinate = {lat: 37.7577, lng: -122.4376, searchQuery: 'sushi'};
 
-function initialize() {
+// ViewModel that defines the data and behavior
+function AppViewModel() {
+  var self = this;
+  var markers = [];
+  self.allLocations = ko.observableArray([]);
+
+  var map = initializeMap();
+  self.map = ko.observable(map);
+}
+
+// Activate knockout.js
+// ko.applyBindings(new AppViewModel());
+google.maps.event.addDomListener(window, 'load', ko.applyBindings(new AppViewModel()));
+
+
+
+
+
+
+function initializeMap() {
     var mapOptions = {
       center: new google.maps.LatLng(initialCoordinate.lat, initialCoordinate.lng),
       zoom: 12,
@@ -11,5 +30,13 @@ function initialize() {
 }
 
 // google.maps.event.addDomListener(window, 'load', initialize);
-google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+// Foursquare
+var forsquareUrl = 'https://api.foursquare.com/v2/venues/search' +
+  '?client_id=MSJE5EGJPP5MD0PRPRERMCU1V4E4MKQRSSH4KCCBUDIF5W5F' +
+  '&client_secret=UJ2DOWO3TDSWOXFEWBGDZY5QOQ4KGWZ3HXVIA54FOB55O1YO' +
+  '&v=20130815' +
+  '&ll=' + initialCoordinate.lat + ',' + initialCoordinate.lng +
+  '&query=' + initialCoordinate.searchQuery;
 
